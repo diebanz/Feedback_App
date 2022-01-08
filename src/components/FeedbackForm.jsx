352eac538887ba586760d75a51d7653b1fa26792
Natalia Banz <banznatalia@gmail.com>
2022-01-08 18:31:33 +0100

    Button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 function FeedbackForm() {
     const [text, setText] = useState("");
@@ -21,7 +22,7 @@ function FeedbackForm() {
                         placeholder="Write a review"
                         value={text}
                     />
-                    <button type="submit">Send</button>
+                    <Button type="submit">Send</Button>
                 </div>
             </form>
         </Card>
