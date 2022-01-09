06f98c07a9aafa0789abdbafda93b91e5e5bd45b
Natalia Banz <banznatalia@gmail.com>
2022-01-09 18:42:05 +0100

    some animation with framer motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import React from "react";
 import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
@@ -10,13 +11,22 @@ function FeedbackList({ feedback, deleteHandler }) {
     }
     return (
         <div className="feedback-list">
-            {feedback.map((item) => (
-                <FeedbackItem
-                    key={item.id}
-                    item={item}
-                    deleteHandler={deleteHandler}
-                />
-            ))}
+            <AnimatePresence>
+                {feedback.map((item) => (
+                    <motion.div
+                        key={item.id}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                    >
+                        <FeedbackItem
+                            key={item.id}
+                            item={item}
+                            deleteHandler={deleteHandler}
+                        />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
         </div>
     );
 }
